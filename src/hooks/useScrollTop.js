f6c331d9ebef5be4790240e20c4efea82abfd47f
Evgeny Shmakov <sjs-master@yandex.ru>
2021-08-06 01:55:25 +0700

    Delete useScrollTop.js

--- src/hooks/useScrollTop.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-const useScrollTop = () => {
-  useEffect(() => {
-    document.body.scrollIntoView({ behavior: "smooth" });
-  }, []);
-};
-
-export default useScrollTop;
