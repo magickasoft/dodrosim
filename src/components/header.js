c95b3ee98870dc2781e388edc8697fb9fd582ff0
Evgeny Shmakov <sjs-master@yandex.ru>
2021-08-07 02:16:10 +0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -16,14 +16,20 @@ const Container = SC.header`
   filter: drop-shadow(0px 0px 20px rgba(51, 62, 102, 0.1)) drop-shadow(0px 0px 40px rgba(51, 62, 102, 0.05));
 `;
 
+const Content = SC.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const Header = () => (
   <Container>
     <HeaderLogo />
-    <div>
+    <Content>
       <NavLink href="/services" name="Услуги" />
       <NavLink href="/objects" name="Объекты" />
       <NavLink href="/about" name="Об учреждении" />
       <NavLink href="/contacts" name="Контакты" />
-    </div>
+    </Content>
   </Container>
 );
