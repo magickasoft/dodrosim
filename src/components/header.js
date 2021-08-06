360ae2f95e7cd27196b37d26b618849792b3b70b
Evgeny Shmakov <sjs-master@yandex.ru>
2021-08-07 03:35:34 +0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -9,27 +9,34 @@ const Container = SC.header`
   left: 0;
   right: 0;
   background: #fff;
+  filter: drop-shadow(0px 0px 20px rgba(51, 62, 102, 0.1)) drop-shadow(0px 0px 40px rgba(51, 62, 102, 0.05));
+`;
+
+const Line = SC.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
   height: 60px;
-  filter: drop-shadow(0px 0px 20px rgba(51, 62, 102, 0.1)) drop-shadow(0px 0px 40px rgba(51, 62, 102, 0.05));
 `;
 
-const Content = SC.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+const menu = [
+  { name: 'Услуги', href: '/services' },
+  { name: 'Объекты', href: '/objects' },
+  { name: 'Об учреждении', href: '/about' },
+  { name: 'Контакты', href: '/contacts' },
+];
 
-export const Header = () => (
-  <Container>
-    <HeaderLogo />
-    <Content>
-      <NavLink href="/services" name="Услуги" />
-      <NavLink href="/objects" name="Объекты" />
-      <NavLink href="/about" name="Об учреждении" />
-      <NavLink href="/contacts" name="Контакты" />
-    </Content>
-  </Container>
-);
+
+export const Header = () => {
+  const items = menu.map(i => <NavLink key={i.name} {...i} />);
+  return (
+    <Container>
+      <Line>
+        <HeaderLogo />
+        <div>
+          {items}
+        </div>
+      </Line>
+    </Container>
+  );
+};
