b4ae94e888fee4b28141886f9fbc8a1d061705a0
Evgeny Shmakov <sjs-master@yandex.ru>
2021-08-10 12:38:31 +0700

    add 404 page

--- src/components/icon/lib/index.js
+++ src/components/icon/lib/index.js
@@ -1,8 +1,10 @@
+import { notFound } from './404';
 import { burger } from './burger';
 import { close } from './close';
 import { logo } from './logo';
 
 export default {
+  notFound,
   burger,
   close,
   logo,
